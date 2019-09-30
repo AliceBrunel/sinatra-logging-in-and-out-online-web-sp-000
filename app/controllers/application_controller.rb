4b54a137e39ab499177d6143a2be569c315d2062
Alice Brunel <alc.brunel@gmail.com>
2019-09-30 10:28:31 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,8 +22,12 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-    
-    erb :acount
+    @current_user = User.find_by_id(session[:user_id])
+    if @current_user
+      erb :account
+    else
+      erb :error
+    end
   end
 
   get '/logout' do
